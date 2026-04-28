05bf6c6154670c4130e71303025c663f32c1152d
Giulia Altamura <giulia.altamura@proton.me>
2026-04-28 08:42:41 +0200

    downloaded the fonts

--- App.tsx
+++ App.tsx
@@ -2,15 +2,14 @@ import './src/i18n';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { BottomTabNavigator } from './src/navigation/BottomTabNavigator';
-import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p';
-import { SpaceMono_400Regular } from '@expo-google-fonts/space-mono';
+import { useFonts } from 'expo-font';
 import { View, ActivityIndicator } from 'react-native';
 import { colors } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    PressStart2P_400Regular,
-    SpaceMono_400Regular,
+    PressStart2P_400Regular: require('./assets/fonts/PressStart2P_400Regular.ttf'),
+    SpaceMono_400Regular: require('./assets/fonts/SpaceMono_400Regular.ttf'),
   });
 
   if (!fontsLoaded) {
